37626597831b465a990f8424a4bc11ebcd4e3030
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2021-01-26 18:05:20 +0300

    Initial Commit

---
+++ Homework 1.2 - Svetofor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 1.2 - Svetofor
+//
+//  Created by Виталий Сабин on 26.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
